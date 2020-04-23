e5b6894f351ea3b01b3d5c70b4e9ee60fa7e2a03
hmightypirate <hcerezoc@gmail.com>
2020-04-23 16:43:51 +0200

    refactoring code

--- gooseka_control/gooseka_commands.py
+++ gooseka_control/gooseka_commands.py
@@ -11,6 +11,36 @@ logger = logging.getLogger(__name__)
 
 class GoosekaCommands(Commands):
     """ Gamepad controller """
+
+    def _limit_with_mppt(self, telemetry, target_left, target_right):
+        """ Duty limitations with MPPT
+
+        Keyword arguments:
+        telemetry -- dict with telemetry information
+        target_left -- target duty for the left motor
+        target_right -- target duty for the right motor
+
+        """
+
+        logger.info("Executing MPTT")
+        # FIXME mppt duty should be modified here
+        total_duty = self.mptt.step(
+            telemetry, self.duty_left + self.duty_right)
+
+        logger.info("Executing MPTT {} {}".format(total_duty, self.duty_left + self.duty_right))
+                    
+        if total_duty < target_left + target_right:
+            new_target_left = ((1.0 * target_left) / (target_left + target_right)) * total_duty
+            new_target_right = ((1.0 * target_right) / (target_left + target_right)) * total_duty
+            
+            target_left = new_target_left
+            target_right = new_target_right
+                        
+            logger.info("LEFT {} RIGHT {}".format(
+                target_left,
+                target_right))
+
+        return target_left, target_right
     
     def _get_acceleration(self, value):
         """ Returns acceleration using the value of the button 
@@ -42,25 +72,28 @@ class GoosekaCommands(Commands):
         if "MOT_SLEWRATE" in self.config and self.config["MOT_SLEWRATE"] > 0:
             if abs(duty - past_duty) > (
                     (self.config["MAX_DUTY"] - self.config["MIN_DUTY"]) *
-                    (1.0/self.config["MOT_SLEWRATE"]) * self.config["LOOP_CONTROL_MS"]/1000.0):
+                    (1.0/self.config["MOT_SLEWRATE"]) *
+                    self.config["LOOP_CONTROL_MS"]/1000.0):
 
                 if duty - past_duty > 0:
                     return past_duty + (
                         (self.config["MAX_DUTY"] - self.config["MIN_DUTY"]) *
-                        (1.0/self.config["MOT_SLEWRATE"]) * self.config["LOOP_CONTROL_MS"]/1000.0)
+                        (1.0/self.config["MOT_SLEWRATE"]) *
+                        self.config["LOOP_CONTROL_MS"]/1000.0)
 
                 else:
                     return past_duty - (
                         (self.config["MAX_DUTY"] - self.config["MIN_DUTY"]) *
-                        (1.0/self.config["MOT_SLEWRATE"]) * self.config["LOOP_CONTROL_MS"]/1000.0)
+                        (1.0/self.config["MOT_SLEWRATE"]) *
+                        self.config["LOOP_CONTROL_MS"]/1000.0)
         return duty
     
-    def _execute_loop_control(self, telemetry, execute_mptt):
+    def _execute_loop_control(self, telemetry, execute_mppt):
         """ Execute the loop control 
 
         Keyword arguments:
         telemetry -- dict with telemetry information
-        execute_mptt -- true if mptt should be calculated
+        execute_mppt -- true if mppt should be calculated
 
         """        
         current_millis = millis()
@@ -94,25 +127,10 @@ class GoosekaCommands(Commands):
                 target_right = self._filter_duty_slew_rate(target_right, self.duty_right)
 
                 logger.info("TARGETSLEW {} {}".format(target_left, target_right))
+                # Limit with MPPT
+                if execute_mppt:
+                    target_left, target_right = self._limit_with_mppt(telemetry, target_left, target_right)
                 
-                if execute_mptt:
-                    logger.info("Executing MPTT")
-                    # FIXME mptt duty should be modified here
-                    total_duty = self.mptt.step(telemetry, self.duty_left + self.duty_right)
-
-                    logger.info("Executing MPTT {} {}".format(total_duty, self.duty_left + self.duty_right))
-                    
-                    if total_duty < target_left + target_right:
-                        new_target_left = ((1.0 * target_left) / (target_left + target_right)) * total_duty
-                        new_target_right = ((1.0 * target_right) / (target_left + target_right)) * total_duty
-
-                        target_left = new_target_left
-                        target_right = new_target_right
-                        
-                        logger.info("LEFT {} RIGHT {}".format(
-                            target_left,
-                            target_right))
-
                 # limit duty to the maximum/minimum accepted
                 target_left = np.clip(target_left, self.config["MIN_DUTY"],
                                     self.config["MAX_DUTY"])
@@ -195,7 +213,7 @@ class GoosekaCommands(Commands):
             
             logger.info("MANUAL")
                         
-        self._execute_loop_control(telemetry, execute_mptt=mppt_flag)
+        self._execute_loop_control(telemetry, execute_mppt=mppt_flag)
 
         code_list.append(self._set_duty_left(self.duty_left))
         code_list.append(self._set_duty_right(self.duty_right))
@@ -216,6 +234,7 @@ class GoosekaCommands(Commands):
         self.btn_deccel = self.config["DECCEL"]
         self.btn_start = self.config["START"]
         self.btn_stop = self.config["STOP"]
+        self.axis_steer = self.config["STEER"]
         
         self.last_button_event = None
         
