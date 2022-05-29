1fe0c0979a9b192dc17f3b668a8b8699868f9113
David Chaves <dchavesd@gmail.com>
2022-05-29 11:42:21 +0200

    Added LIMPING state. Max duty can be modified with DPAD up & down

--- gooseka_control/auto_commands.py
+++ gooseka_control/auto_commands.py
@@ -10,17 +10,19 @@ from .utils import millis
 STATE_STOP = 0x00
 STATE_STARTING = 0x01
 STATE_MAXPOWER = 0x02
+STATE_LIMPING = 0x04
 
 # State machine diagram https://docs.google.com/drawings/d/1Mk_Xc0m1AX4f9dR5dXItTYfNQN611EUfX3osR9mFplU/edit
 BTN_A = "BTN_SOUTH"
 BTN_B = "BTN_EAST"
 BTN_Y = "BTN_NORTH"
+BTN_X = "BTN_WEST"
 
-LIGHT_TURN = 0.1
-STANDARD_TURN = 0.25
+LIGHT_TURN = 0.05
+STANDARD_TURN = 0.1
 HARD_TURN = 0.4
 
-COMMAND_SLOWDOWN = 50
+COMMAND_SLOWDOWN = 10
 
 logger = logging.getLogger(__name__)
 
@@ -43,7 +45,7 @@ class AutoCommands(Commands):
 
         if events is not None:            
             for event in events:
-                # logger.info("CODE: {:12} STATE:{}".format(event.code, event.state))
+                # print("CODE: {:12} STATE:{}".format(event.code, event.state))
                 if(event.code == "BTN_TL"):
                     self.light_turn_left = (event.state == 1)
                 if(event.code == "BTN_TR"):
@@ -61,6 +63,8 @@ class AutoCommands(Commands):
                     code_list = self.state_starting(telemetry, event.code, event.state)
                 elif (self.state == STATE_MAXPOWER):
                     code_list = self.state_maxpower(telemetry, event.code, event.state)
+                elif (self.state == STATE_LIMPING):
+                    code_list = self.state_limping(telemetry, event.code, event.state)
 
         return code_list
 
@@ -99,7 +103,7 @@ class AutoCommands(Commands):
         if(self.turn_right | self.light_turn_right):
             self.angular_duty = 255
         self.linear_duty = 40 if self.go_straight else 0
-        logger.info("LINEAR: {:>3}\tANGULAR: {:>3}".format(int(self.linear_duty), int(self.angular_duty)))
+        print("\rLINEAR: {:>3}\tANGULAR: {:>3}".format(int(self.linear_duty), int(self.angular_duty)), end='')
         # Send commands
         code_list.append(self._set_duty_angular(self.angular_duty)) # Always starts in a straight line
         code_list.append(self._set_duty_linear(self.linear_duty)) # TODO Change fixed value to adaptive based on telemetry
@@ -111,8 +115,21 @@ class AutoCommands(Commands):
         turning_modifier = self.calculate_turn()
         
         self.angular_duty = int(round(128.0 * (1.0 + turning_modifier)))
-        self.linear_duty = 255
-        logger.info("LINEAR: {:>3}\tANGULAR: {:>3}".format(int(round(self.linear_duty)), int(round(self.angular_duty))))
+        self.linear_duty = self.maxpower_duty
+        print("\rLINEAR: {:>3}\tANGULAR: {:>3}".format(int(round(self.linear_duty)), int(round(self.angular_duty))), end='')
+        # Send commands
+        code_list.append(self._set_duty_linear(self.linear_duty))
+        code_list.append(self._set_duty_angular(self.angular_duty))
+        return code_list
+
+    def get_limping_command(self, telemetry, code, state):
+        code_list = []
+        
+        turning_modifier = self.calculate_turn()
+        
+        self.angular_duty = int(round(128.0 * (1.0 + turning_modifier)))
+        self.linear_duty = self.limping_duty
+        print("\rLINEAR: {:>3}\tANGULAR: {:>3}".format(int(round(self.linear_duty)), int(round(self.angular_duty))), end='')
         # Send commands
         code_list.append(self._set_duty_linear(self.linear_duty))
         code_list.append(self._set_duty_angular(self.angular_duty))
@@ -134,13 +151,13 @@ class AutoCommands(Commands):
         if (code == BTN_A):
             self.set_led(0x01)
             self.state = STATE_STARTING
-            print("STATE STARTING")
+            print("\nSTATE STARTING")
         if (millis() - self.last_sent_command) < COMMAND_SLOWDOWN: # SEND COMMANDS AT MOST ONCE EVERY COMMAND_SLOWDOWN MS
             return []
         self.last_sent_command = millis()
         self.angular_duty = 128
         self.linear_duty = 0
-        logger.info("LINEAR: {:>3}\tANGULAR: {:>3}".format(int(round(self.linear_duty)), int(round(self.angular_duty))))
+        print("\rLINEAR: {:>3}\tANGULAR: {:>3}".format(int(round(self.linear_duty)), int(round(self.angular_duty))), end='')
         code_list.append(self._set_duty_linear(self.linear_duty))
         code_list.append(self._set_duty_angular(self.angular_duty))
         return code_list
@@ -149,12 +166,15 @@ class AutoCommands(Commands):
         if (code == BTN_B):
             self.set_led(0x02)
             self.state = STATE_MAXPOWER
-            print("STATE MAXPOWER")
+            print("\nSTATE MAXPOWER")
         if (code == BTN_Y):
             self.set_led(0x00)
             self.state = STATE_STOP
-            print("STATE STOP")
-        
+            print("\nSTATE STOP")
+        if (code == BTN_X):
+            self.set_led(0x04)
+            self.state = STATE_LIMPING
+            print("\nSTATE LIMPING")
         if (millis() - self.last_sent_command) < COMMAND_SLOWDOWN: # SEND COMMANDS AT MOST ONCE EVERY COMMAND_SLOWDOWN MS
             return []
         self.last_sent_command = millis()
@@ -164,15 +184,49 @@ class AutoCommands(Commands):
         if (code == BTN_A):
             self.set_led(0x01)
             self.state = STATE_STARTING
-            print("STATE STARTING")
+            print("\nSTATE STARTING")
         if (code == BTN_Y):
             self.set_led(0x00)
             self.state = STATE_STOP
-            print("STATE STOP")
+            print("\nSTATE STOP")
+        if (code == BTN_X):
+            self.set_led(0x04)
+            self.state = STATE_LIMPING
+            print("\nSTATE LIMPING")
+        
+        if ((code == "BTN_DPAD_UP") & (state == 1)):
+            self.maxpower_duty = constrain(self.maxpower_duty + 20, 100, 255)
+        if ((code == "BTN_DPAD_DOWN") & (state == 1)):
+            self.maxpower_duty = constrain(self.maxpower_duty - 20, 100, 255)
+
         if (millis() - self.last_sent_command) < COMMAND_SLOWDOWN: # SEND COMMANDS AT MOST ONCE EVERY COMMAND_SLOWDOWN MS
             return []
         self.last_sent_command = millis()
         return self.get_maxpower_command(telemetry, code, state)
+
+    def state_limping(self, telemetry, code, state):
+        if (code == BTN_A):
+            self.set_led(0x01)
+            self.state = STATE_STARTING
+            print("\nSTATE STARTING")
+        if (code == BTN_Y):
+            self.set_led(0x00)
+            self.state = STATE_STOP
+            print("\nSTATE STOP")
+        if (code == BTN_B):
+            self.set_led(0x02)
+            self.state = STATE_MAXPOWER
+            print("\nSTATE MAXPOWER")
+
+        if ((code == "BTN_DPAD_UP") & (state == 1)):
+            self.limping_duty = constrain(self.limping_duty + 10, 20, 100)
+        if ((code == "BTN_DPAD_DOWN") & (state == 1)):
+            self.limping_duty = constrain(self.limping_duty - 10, 20, 100)
+        
+        if (millis() - self.last_sent_command) < COMMAND_SLOWDOWN: # SEND COMMANDS AT MOST ONCE EVERY COMMAND_SLOWDOWN MS
+            return []
+        self.last_sent_command = millis()
+        return self.get_limping_command(telemetry, code, state)
 
     def __init__(self, config):
         """ Initialization """
@@ -185,6 +239,8 @@ class AutoCommands(Commands):
         self.turn_left = False
         self.turn_right = False
         self.go_straight = False
+        self.maxpower_duty = 140
+        self.limping_duty = 30
         self.set_led(0x00)
         self.last_sent_command = 0
         print("STATE STOP")
